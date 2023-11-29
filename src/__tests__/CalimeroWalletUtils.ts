b4d2b45dad03370875a4465d0e98f74bf2eb4d9d
ijerkovic <igor@calimero.network>
2023-11-29 15:39:21 +0100

    Methods for syncing account and generating the x-signature
    Test plan:
    ran node index.js in examples/
    and verified provided account gets synced to Calimero shard
    and that signed txs can be sent

---
+++ src/__tests__/CalimeroWalletUtils.ts
@@ -0,0 +1,24 @@
+import { CalimeroWalletUtils } from '../index';
+import { KeyPair, InMemorySigner } from 'near-api-js';
+
+test('Verify signature for challenge', async () => {
+  const config = {
+    shardId: '',
+    rpcEndpoint: '',
+    calimeroUrl: '',
+  };
+  const walletUtils = CalimeroWalletUtils.init(config);
+
+  const PRIVATE_KEY =
+    'ed25519:5bftebifibPGnKrQ1p9EMgfzKzpymWjnDEkDf9Rv1ZYa8TuiTqb8XKX2Fp722grWYqtGLhz8gdYpgxekrjGWoS9B';
+  const ACCOUNT_ID = 'mockwallet.testnet';
+
+  const keyPair = KeyPair.fromString(PRIVATE_KEY);
+  const signer = await InMemorySigner.fromKeyPair(config.shardId, ACCOUNT_ID, keyPair);
+
+  let sibObj = await walletUtils.signatureForChallenge(ACCOUNT_ID, signer, 'a mock challenge');
+
+  expect(
+    sibObj.signature.toBe('2V0NPo9caL3oTSSbrHjRKbxM8dgmEBc2A1pZ3hGg6lNTAQYBzPLYu2KtPrKgZAYQ0Z2Xe3jSvKiC4PZ8rwTPBg=='),
+  );
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,102 @@
-export const CalimeroWalletUtils = (version: string) => `Version ${version}`;
+import { keyStores, Near } from 'near-api-js';
+import * as NearAPI from 'near-api-js';
+
+export interface ICalimeroConfig {
+  shardId: string;
+  calimeroUrl: string;
+  rpcEndpoint: string;
+}
+
+export class CalimeroWalletUtils {
+  public static init(config: ICalimeroConfig): CalimeroWalletUtils {
+    return new CalimeroWalletUtils(config);
+  }
+
+  private config: ICalimeroConfig;
+
+  private constructor(config: ICalimeroConfig) {
+    this.config = config;
+  }
+
+  public async fetchChallenge() {
+    const response = await fetch(`${this.config.calimeroUrl}/api/public/challenge`, {
+      headers: {
+        'Content-type': 'application/json; charset=utf-8',
+      },
+      method: 'POST',
+    });
+    if (!response.ok) {
+      const body = await response.text();
+      let parsedBody;
+
+      try {
+        parsedBody = JSON.parse(body);
+      } catch (e) {
+        throw new Error(`${response.status} ${body}`);
+      }
+
+      throw new Error(`${response.status}, ${parsedBody}`);
+    }
+    return await response.json();
+  }
+
+  public async signatureForChallenge(accountId: string, signer: NearAPI.Signer, challenge: string) {
+    const signed = await signer.signMessage(Buffer.from(challenge), accountId, this.config.shardId);
+    const signature = Buffer.from(signed.signature).toString('base64');
+    const publicKey = signed.publicKey.toString();
+
+    return { challenge, signature, publicKey, accountId };
+  }
+
+  public async generatePrivateShardXSignature(accountId: string, signer: NearAPI.Signer) {
+    const challenge = await this.fetchChallenge();
+    const signedChallenge = await this.signatureForChallenge(accountId, signer, challenge.data);
+    const encodedSig = Buffer.from(JSON.stringify(signedChallenge)).toString('base64');
+    return encodedSig;
+  }
+
+  public async syncPrivateShardAccount(accountId: string, signer: NearAPI.Signer) {
+    const xSignature = await this.generatePrivateShardXSignature(accountId, signer);
+    await this.syncAccount(accountId, xSignature);
+  }
+
+  public async getCalimeroConnection(keyStore: keyStores.KeyStore, xSignature: string): Promise<Near> {
+    const calimero = await NearAPI.connect({
+      headers: {
+        'x-signature': xSignature,
+      },
+      keyStore,
+      networkId: this.config.shardId,
+      nodeUrl: this.config.rpcEndpoint,
+    });
+    return calimero;
+  }
+
+  private async syncAccount(accountId: string, xSignature: string) {
+    const postData = {
+      accountId,
+      shardId: this.config.shardId,
+    };
+    const response = await fetch(`${this.config.calimeroUrl}/api/public/sync`, {
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-type': 'application/json; charset=utf-8',
+        'x-signature': xSignature,
+      },
+      method: 'POST',
+    });
+    if (!response.ok) {
+      const body = await response.text();
+      let parsedBody;
+
+      try {
+        parsedBody = JSON.parse(body);
+      } catch (e) {
+        throw new Error(`${response.status} ${body}`);
+      }
+
+      throw new Error(`${response.status}, ${parsedBody}`);
+    }
+    return await response.json();
+  }
+}
